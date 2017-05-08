18492f932588ec59878936460b3142027b62c2a8
Anastasiya Yuragina <dark25555@gmail.com>
2017-05-08 15:54:41 +0300

    make designe

---
+++ app/src/main/java/com/anastasiyayuragina/timer/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.anastasiyayuragina.timer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun playTimer() {
+
+    }
+
+    fun add5seconds() {
+
+    }
+}
